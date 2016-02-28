409caf4c16bf3400fd4a7615686f67275b1e7a6b
chocochipcookiie <anna@changfamily.org>
2016-02-28 03:35:35 -0800

    newjsfile

--- js/onClick.js
+++ js/onClick.js
@@ -2,16 +2,18 @@ $(document).ready(function(){
  
     $('.navitems li a').click(function(event){
         console.log("Currently in earthquake.js");
-        event.preventDefault();
+        
         var url = $(this).attr('href');
         console.log("Type: " + $(this).attr('type'));
         switch($(this).attr('type'))
             {
                 case('application/mp4'):
+                    event.preventDefault();
                     console.log('You are a video...');
                     $('#content').html('<video src="' + url + '" controls></video>');
                     break;
                 case('image/jpeg'):
+                    event.preventDefault();
                     console.log('You are an image...');
                     $('#content').html('<img src="' + url + '">')
                     break;
